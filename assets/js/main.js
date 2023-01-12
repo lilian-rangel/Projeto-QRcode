ed675252bd17bd04de9604a5194ccc559f90983f
lilian-rangel <lilianrm.nutri@gmail.com>
2023-01-12 12:07:28 -0300

    :bug: fix: fix the js error prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,28 @@
 const input = document.querySelector("input");
+const searchBtn = document.querySelector(".search");
 const qrcode = document.querySelector("#qrcode-img");
 
-document.addEventListener("keypress", (e) => {
-    if(e.key === "Enter") {
-        genQRCode();
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(!input.value) {
+        modal.classList.add("open-modal");
+        closeBtn.addEventListener("click", function () {
+            modal.classList.remove("open-modal");
+        });
+        return false;
     }
+
+    genQRCode();
 });
 
 function genQRCode() {
     if (!input.value) return;
 
     qrcode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+    input.value= "";
 }
